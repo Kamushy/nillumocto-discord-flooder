111a149904333690fb0c62f36b2ef42be2e33a22
Kamushy <92086533+Kamushy@users.noreply.github.com>
2022-04-03 13:33:34 +1000

    Added a command that renames all member

--- nillumocto.py
+++ nillumocto.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 client = commands.Bot(command_prefix=".")
 
 #read github description 
+servernickname = "Enter what you want to re name server here"
 
 @client.command()
 async def octo(ctx):
@@ -29,6 +30,20 @@ async def delete(ctx):
     guild = ctx.guild
     for channel in guild.text_channels:
         await channel.delete()
+                
+@client.command()
+async def rename(ctx):
+    
+    guild = ctx.guild
+    await ctx.reply("Renaming all memembers")
+    for person in guild.members:
+        
+        if person == ctx.guild.owner:
+            print("Cant change since its the guild owner")
+        try:     
+            await person.edit(nick=servernickname)
+        except:
+            print(F"{person} username could not be changed try giving the bot a higher role")
 
 #Add token here, read github description if confused
 client.run('add your token here')
