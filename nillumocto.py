9de40af6a0335255fd4d1d176b36fd96fd82cf35
Kamushy <92086533+Kamushy@users.noreply.github.com>
2022-04-03 13:34:20 +1000

    Changed embed

--- nillumocto.py
+++ nillumocto.py
@@ -19,7 +19,7 @@ async def octo(ctx):
         channel = await guild.create_text_channel(f"flooded-by, {username}")
 
         em = discord.Embed(title = "Nillumocto", description = "\n", color=0x0ff41)
-        em.add_field(name="https://github.com/Kamushy/nillumocto", value="Coded by kamushy" )
+        em.add_field(name="https://github.com/Kamushy/nillumocto-discord-flooder", value="Coded by kamushy" )
         await channel.send(embed = em)
         await channel.send("@everyone")
 
